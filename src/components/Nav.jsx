2fb5a928c1599dc3544466955c638bbbd159b18b
ferutizi <fer.utizi@gmail.com>
2022-10-25 15:53:51 -0300

    corrigiendo vw de navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,14 +14,13 @@ const Nav = () => {
                     <a href='https://github.com/ferutizi' target='_blank'>
                         <img className='nav--icon' src={require('../img/icons/github.png')}></img>
                     </a>
-                    <a>
+                    <a href='https://www.linkedin.com/in/fernando-utizi-2a72a3233/' target='_blank'>
                         <img className='nav--icon' src={require('../img/icons/linkedin.png')}></img>
                     </a>
                 </div>
                 <div className='nav--darkmode'>
                     <img className='nav--icon' src={require('../img/icons/dark.png')}></img>
                 </div>
-                {/* github linkedin nightmode */}
             </div>
         </nav>
     )
