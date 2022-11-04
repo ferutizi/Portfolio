323a07f65870174262a45d41f779e22527d0cbf3
ferutizi <fer.utizi@gmail.com>
2022-11-04 20:01:23 -0300

    validaciones formulario

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,6 @@ import { useState } from 'react';
 import Form from './Form';
 
 const Footer = () => {
-    const [mail, setMail] = useState();
-
     return(
         <div>
             <Form />
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,26 @@
 import './Form.scss';
 import { useFormik } from 'formik';
+import { useState } from 'react';
 
-const Form = ({ setMail }) => {
+const validate = (values) => {
+    const errors = {}
+    if(!errors.name) {
+        errors.name = 'requerido'
+    }
+    if(!errors.email) {
+        errors.email = 'requerido'
+    }
+    if(!errors.subject) {
+        errors.subject = 'requerido'
+    }
+    if(!errors.message) {
+        errors.message = 'requerido'
+    }
+    return errors;
+}
 
+const Form = () => {
+    const [mail, setMail] = useState();
 
     const contacto = useFormik({
         initialValues: {
@@ -11,27 +29,39 @@ const Form = ({ setMail }) => {
             subject: '',
             message: ''
         },
-        onSubmit: values => setMail(values)
-    })
+        validate,
+        onSubmit: (values, {resetForm}) => {
+            setMail(values);
+            resetForm({values: ''});
+        }
+    });
 
     return(
         <form onSubmit={contacto.handleSubmit}>
             <input
                 type='text'
                 placeholder='Nombre'
-                {...contacto.getFieldProps('name')} />
+                {...contacto.getFieldProps('name')} 
+            />
+            {contacto.errors.name ? <div style={{color: 'red'}}>{contacto.errors.name}</div> : null}
             <input
                 type='email'
                 placeholder='E-mail'
-                {...contacto.getFieldProps('email')} />
+                {...contacto.getFieldProps('email')} 
+            />
+            {contacto.errors.email ? <div>{contacto.errors.email}</div> : null}
             <input
                 type='text'
                 placeholder='Asunto'
-                {...contacto.getFieldProps('subject')} />
+                {...contacto.getFieldProps('subject')} 
+            />
+            {contacto.errors.subject ? <div>{contacto.errors.subject}</div> : null}
             <input
                 type='text-area'
                 placeholder='Mensaje'
-                {...contacto.getFieldProps('message')} />
+                {...contacto.getFieldProps('message')} 
+            />
+            {contacto.errors.message ? <div>{contacto.errors.message}</div> : null}
             <button type='submit'>Enviar</button>
         </form>
     )
