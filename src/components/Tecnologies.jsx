253e74675de6c849057674358a16d33853299941
ferutizi <fer.utizi@gmail.com>
2023-01-21 19:25:46 -0300

    mapeando tecnologies

--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -1,4 +1,5 @@
 import Tecnology from "./Tecnology";
+import tecnologies from "../Tecnologies.json";
 import './Tecnology.scss';
 
 const Tecnologies = () => {
@@ -6,18 +7,9 @@ const Tecnologies = () => {
         <>
             <h3 className="subtitle">Tecnologías</h3>
             <div className="tecnology__container">
-                <Tecnology tecnology={'react'} text={'React'} />
-                <Tecnology tecnology={'redux'} text={'Redux'}/>
-                <Tecnology tecnology={'react-router'} text={'React Router'}/>
-                <Tecnology tecnology={'testing-library'} text={'Testing Library'}/>
-                <Tecnology tecnology={'css'} text={'CSS'}/>
-                <Tecnology tecnology={'sass'} text={'Sass'}/>
-                <Tecnology tecnology={'bootstrap'} text={'Bootstrap'}/>
-                <Tecnology tecnology={'js'} text={'JavaScript'}/>
-                <Tecnology tecnology={'html'} text={'HTML'} />
-                <Tecnology tecnology={'github_2'} text={'GitHub'} />
-                <Tecnology tecnology={'firebase'} text={'Firebase'} />
-                <Tecnology tecnology={'mySQL'} text={'MySQL'} />
+                {tecnologies.map(item => 
+                    <Tecnology key={item.id} tecnology={item.tecnology} text={item.text} />
+                )}
             </div>
         </>
     )
