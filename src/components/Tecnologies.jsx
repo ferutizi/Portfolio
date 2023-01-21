5f45aea286f38b22317064280dd41f784cf8c752
ferutizi <fer.utizi@gmail.com>
2023-01-21 19:07:05 -0300

    agragando nuevas tecnologias

--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -8,12 +8,15 @@ const Tecnologies = () => {
             <div className="tecnology__container">
                 <Tecnology tecnology={'react'} text={'React'} />
                 <Tecnology tecnology={'redux'} text={'Redux'}/>
+                <Tecnology tecnology={'react-router'} text={'React Router'}/>
+                <Tecnology tecnology={'testing-library'} text={'Testing Library'}/>
                 <Tecnology tecnology={'css'} text={'CSS'}/>
                 <Tecnology tecnology={'sass'} text={'Sass'}/>
                 <Tecnology tecnology={'bootstrap'} text={'Bootstrap'}/>
                 <Tecnology tecnology={'js'} text={'JavaScript'}/>
                 <Tecnology tecnology={'html'} text={'HTML'} />
                 <Tecnology tecnology={'github_2'} text={'GitHub'} />
+                <Tecnology tecnology={'firebase'} text={'Firebase'} />
                 <Tecnology tecnology={'mySQL'} text={'MySQL'} />
             </div>
         </>
