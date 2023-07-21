afd1aac349b7a1fb9df5ee03bf7c3a2d59e4c7d3
ferutizi <fer.utizi@gmail.com>
2023-07-21 18:50:16 -0300

    updated cv

--- src/components/IconsNav.jsx
+++ src/components/IconsNav.jsx
@@ -9,7 +9,7 @@ const IconsNav = ({ cvOk, setCvOk }) => {
       <a href='https://www.linkedin.com/in/fernando-utizi-2a72a3233/' target='_blank' rel='noreferrer'>
         <img className='nav--icon' src={require('../img/icons/linkedin.png')} />
       </a>
-      <a href='Fernando Utizi - FrontEnd Developer - CV.pdf' download='Fernando Utizi-FrontEnd Developer-CV.pdf' target='_blank' rel='noreferrer'>
+      <a href='Fernando Utizi - FrontEnd Developer - CV.pdf' download='Fernando Utizi - FrontEnd Developer - CV.pdf' target='_blank' rel='noreferrer'>
         <img className='nav--icon' src={require(`../img/icons/${cvOk ? 'cv_ok' : 'cv'}.png`)} onClick={() => setCvOk(true)} />
       </a>
     </>
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -16,7 +16,7 @@ const Presentation = ({ cvOk, setCvOk }) => {
           <a href='https://www.linkedin.com/in/fernando-utizi-2a72a3233/' target='_blank' rel='noreferrer'>
             <img className='nav--icon' src={require('../img/icons/linkedin.png')} />
           </a>
-          <a href='Fernando Utizi-FrontEnd Developer-CV.pdf' download='Fernando Utizi-FrontEnd Developer-CV.pdf' target='_blank' rel='noreferrer'>
+          <a href='Fernando Utizi - FrontEnd Developer - CV.pdf' download='Fernando Utizi - FrontEnd Developer - CV.pdf' target='_blank' rel='noreferrer'>
             <img className='nav--icon' src={require(`../img/icons/${cvOk ? 'cv_ok' : 'cv'}.png`)} onClick={() => setCvOk(true)} />
           </a>
         </div>
