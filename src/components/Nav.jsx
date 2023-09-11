a56aa6bbdcbc9ba0efbb89810c723d17318dea9b
ferutizi <fer.utizi@gmail.com>
2023-09-10 21:39:12 -0300

    changed presentation styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import './Nav.scss'
 const Nav = ({ cvOk, setCvOk }) => {
   return (
     <nav className='nav'>
-      <a href='#home' className='logo'>Fernando Utizi</a>
+{/*       <a href='#home' className='logo'>Fernando Utizi</a> */}
       <div className='sections'>
         <a href='#proyects' className='nav--button'>Proyectos</a>
         <div className='nav--button__div' />
@@ -12,11 +12,11 @@ const Nav = ({ cvOk, setCvOk }) => {
         <div className='nav--button__div' />
         <a href='#contact' className='nav--button'>Contacto</a>
       </div>
-      <div className='icons'>
+{/*       <div className='icons'>
         <div className='nav--group'>
           <IconsNav cvOk={cvOk} setCvOk={setCvOk} />
         </div>
-      </div>
+      </div> */}
     </nav>
   )
 }
