1de1bc41df1a890a923c873ac54e4c69e48353e2
ferutizi <fer.utizi@gmail.com>
2024-02-20 01:17:27 -0300

    experience card style - responsive

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,42 +1,48 @@
+import './Experience.scss'
 import ProyectsRepo from "./ProyectsRepo"
 import Icon from "./Icon"
 
 const Experience = () => {
     return(
         <section>
-            <h3 id='experience' className='subtitle'>Experiencia</h3>
-            <div className='proyect__container'>
-                <div className='proyect__card--experience'>
-                    <h3 className='proyect__title'>Trémolo - Escuela de guitarra</h3>
+            <h3 className='subtitle'>Experiencia</h3>
+            <div className='experience__container'>
+                <div className='experience__card'>
                     <a href='https://www.tremoloescuela.com/' target='_blank' rel='noreferrer'>
                     <img
-                        className='proyect__img--experience'
+                        className='experience__img'
                         src={require('../img/tremolo.webp')}
                         alt= 'Trémolo escuela de guitarra'
                     />
                     </a>
-                    <p className='proyect__text'>Página publicitaria con sistema de inscripción online. Desplegada en vercel con dominio propio </p>
-                    <div className='proyect__icons'>
-                        <div className='proyect__icons--left'>
-                            <Icon
-                                className='icon'
-                                src={require('../img/icons/react.webp')}
-                                alt='react'
-                            />
-                            <Icon
-                                src={require('../img/icons/react-router.webp')}
-                                alt='react router'
-                            />
-                            <Icon
-                                src={require('../img/icons/sass.webp')}
-                                alt='sass'
-                            /> 
-                        </div>
-                        <div className='proyect__icons--right'>
-                            <ProyectsRepo
-                                repository='https://github.com/ferutizi/tremolo-escuela-guitarra'
-                                deploy='https://www.tremoloescuela.com/'
-                            />
+                    <div className='details__container'>     
+                        <h3 className='experience__title'>Trémolo - Escuela de guitarra</h3>
+                        <p className='experience__text'>
+                            Página publicitaria con sistema de inscripción online, diseño
+                            propio y paginación con React Router. Implementación de google maps. 
+                            Desplegada en vercel con dominio propio 
+                        </p>
+                        <div className='experience__icons'>
+                            <div className='experience__icons--left'>
+                                <Icon
+                                    src={require('../img/icons/react.webp')}
+                                    alt='react'
+                                />
+                                <Icon
+                                    src={require('../img/icons/react-router.webp')}
+                                    alt='react router'
+                                />
+                                <Icon
+                                    src={require('../img/icons/sass.webp')}
+                                    alt='sass'
+                                /> 
+                            </div>
+                            <div className='proyect__icons--right'>
+                                <ProyectsRepo
+                                    repository='https://github.com/ferutizi/tremolo-escuela-guitarra'
+                                    deploy='https://www.tremoloescuela.com/'
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/ProyectsRepo.jsx
+++ src/components/ProyectsRepo.jsx
@@ -3,14 +3,14 @@ import './ProyectsRepo.scss'
 const ProyectsRepo = ({ repository, deploy }) => {
   return (
     <>
-      <a href={repository} target='_blank' rel='noreferrer'>
+      <a className='a--experience' href={repository} target='_blank' rel='noreferrer'>
         <img
           className='icon'
           src={require('../img/icons/github.webp')}
           alt='github'
         />
       </a>
-      <a href={deploy} target='_blank' rel='noreferrer'>
+      <a className='a--experience' href={deploy} target='_blank' rel='noreferrer'>
         <img
           className='icon'
           src={require('../img/icons/deploy.webp')}
